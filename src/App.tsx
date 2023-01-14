e78b52bee7030f6056f328d9efafbbcd2a374b3e
Satoshi Nishinaka <satoshie.sp@gmail.com>
2023-01-14 20:56:41 +0900

    だいたいのデザインを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+const ResetButton = (): JSX.Element => {
+    return (<button>RESET</button>)
+}
+
+const StartPauseButton = (props: {running: boolean}): JSX.Element => {
+    if (!props.running) {
+        return (<button>START</button>)
+    }
+
+    return (<button>PAUSE</button>)
+}
+
+const elapse = (running: boolean, startTime: number): string => {
+    if (!running) {
+        return '--:--:--';
+    }
+    return '--:--:--';
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      2023/01/14
-    </div>
-  );
+    const [startTime, setStartTime] = useState<number>(0)
+    const [running, setRunning] = useState(false)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/> Stop Watch
+            </header>
+            <section className='main'>
+                <div>
+                    <div className='timer'>
+                        {elapse(running, startTime)}
+                    </div>
+                </div>
+                <div className='buttons'>
+                <ResetButton /><StartPauseButton running={running} />
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
