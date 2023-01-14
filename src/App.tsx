3fa186615c3219e9fc5eb4083605061328d1e063
Satoshi Nishinaka <satoshie.sp@gmail.com>
2023-01-14 21:45:10 +0900

    インデント修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.scss';
 import {useStopwatch} from "react-timer-hook";
 
-const ResetButton = (props: {clickEvent:()=>void}): JSX.Element => {
+const ResetButton = (props: {clickEvent:() => void}): JSX.Element => {
     return (<button onClick={props.clickEvent}>RESET</button>)
 }
 
@@ -15,11 +15,11 @@ const StartPauseButton = (props: {running: boolean, clickEvent: () => void}): JS
     return (<button onClick={props.clickEvent}>PAUSE</button>)
 }
 
-    const elapse = (running: boolean, hours: number, minutes: number, seconds:number): string => {
+const elapse = (running: boolean, hours: number, minutes: number, seconds:number): string => {
 
-    return hours.toString().padStart(2, '0')
-        + ':' + minutes.toString().padStart(2, '0')
-        + ':' + seconds.toString().padStart(2, '0');
+return hours.toString().padStart(2, '0')
+    + ':' + minutes.toString().padStart(2, '0')
+    + ':' + seconds.toString().padStart(2, '0');
 }
 
 function App() {
@@ -45,12 +45,16 @@ function App() {
                     </div>
                 </div>
                 <div className='buttons'>
-                    <ResetButton clickEvent={reset} />
+                    <ResetButton clickEvent={() => {
+                        // 2回ボタンを押すとちゃんとリセットされるんだけど…
+                        reset();
+                        pause();
+                    }} />
                     <StartPauseButton running={isRunning} clickEvent={() => {
                         if (isRunning) {
                             pause();
                         } else {
-                            start()
+                            start();
                         }
                     }} />
                 </div>
